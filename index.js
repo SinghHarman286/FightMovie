8062ec04fd3a5f09fe2474de0b5137d21468990a
Harman Singh <h286sing@uwaterloo.ca>
2020-07-28 00:52:02 -0600

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const fetchData = async (value) => {
     return response.data.Search
 }
 
-const parent = document.querySelector(".autocomplete");
+const parent = document.querySelector("#autocomplete");
 parent.innerHTML = `
 <label><b>Search For a Movie</b></label>
 <input class = "input">
@@ -44,11 +44,63 @@ const onInput = async event => {
         <img src = "${imageSRC}">
         ${movie.Title}
         `
+        option.addEventListener("click", () => {
+            dropdown.classList.remove("is-active");
+            input.value = movie.Title;
+            onMovie(movie);
+        })
         resultWrapper.appendChild(option);   
     }
-
-
 }
+
+const onMovie = async movie => {
+    const response = await axios.get("https://www.omdbapi.com/", {
+        params: {
+            apikey: "9fad960",
+            i: movie.imdbID
+        }
+    });
+    
+    document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+}
+
+const movieTemplate = movieDetail => {
+    console.log(movieDetail);
+    return `
+        <article class = "media">
+            <figure class = "media-left">
+                <p class = "image">
+                    <img src = "${movieDetail.Poster}">
+                </p>
+            </figure>
+            <div class = "media-content">
+                <div class = "content">
+                    <h1> ${movieDetail.Title}</h1>
+                    <h4> ${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class = "notification is-primary">
+            <p class = "title">${movieDetail.Awards}</p>
+            <p class = "subititle">Awards</p>
+        </article>
+        <article class = "notification is-primary">
+            <p class = "title">${movieDetail.BoxOffice}</p>
+            <p class = "subititle">Box Office</p>
+        </article>
+        <article class = "notification is-primary">
+            <p class = "title">${movieDetail.Metascore}</p>
+            <p class = "subititle">Metascore</p>
+        </article>
+        <article class = "notification is-primary">
+            <p class = "title">${movieDetail.imdbVotes}</p>
+            <p class = "subititle">imdb Votes</p>
+        </article>
+    `;
+}
+
+
 
 input.addEventListener("input", debounce(onInput, 500));
 
